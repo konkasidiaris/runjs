0479f35cbad3c56f45307b8c7027b76c06cc9ae6
Konstantinos Kasidiaris <kkasidiaris@gmail.com>
2022-08-21 15:31:50 +0300

    feat: implements file system read/write/delete bindings to rust

--- example.js
+++ example.js
@@ -1,2 +1,17 @@
 console.log("Hello runjs!");
 console.error("Boom!!")
+
+const path = "./log.txt";
+
+try {
+  const contents = await runjs.readFile(path);
+  console.log("Read from a file", contents);
+} catch (err) {
+  console.error("Unable to read file", path, err);
+}
+
+await runjs.writeFile(path, "I can write to a file.");
+const contents = await runjs.readFile(path);
+console.log("Read from a file", path, "contents: ", contents);
+runjs.removeFile(path);
+console.log("File removed");
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,49 @@
 use deno_core::error::AnyError;
+use deno_core::op;
+use deno_core::Extension;
 use std::rc::Rc;
+
+#[op]
+async fn op_read_file(path: String) -> Result<String, AnyError> {
+    let contents = tokio::fs::read_to_string(path).await?;
+    Ok(contents)
+}
+
+#[op]
+async fn op_write_file(path: String, contents: String) -> Result<(), AnyError> {
+    tokio::fs::write(path, contents).await?;
+    Ok(())
+}
+
+#[op]
+fn op_remove_file(path: String) -> Result<(), AnyError> {
+    std::fs::remove_file(path)?;
+    Ok(())
+}
 
 /// Encapsulates the whole life-cycle that our JavaScript code will go through
 ///
-/// 1. creates a new instance of JsRuntime which uses a file-stystem based module loader
-/// 2. loads a module into runtime
-/// 3. evaluates the module
-/// 4. runs the event loop to completion
+/// 1. creates a js extension to configure our instance of JsRuntime
+///   a. expose different Rust functions
+///   b. load additional JavaScript code etc
+/// 2. creates a new instance of JsRuntime which uses a file-stystem based module loader
+/// 3. loads extensions and a module into runtime
+/// 4. evaluates the module
+/// 5. runs the event loop to completion
 async fn run_js(file_path: &str) -> Result<(), AnyError> {
     let main_module = deno_core::resolve_path(file_path)?;
+
+    let runjs_extension = Extension::builder()
+        .ops(vec![
+            op_read_file::decl(),
+            op_write_file::decl(),
+            op_remove_file::decl(),
+        ])
+        .build();
+
     let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
         module_loader: Some(Rc::new(deno_core::FsModuleLoader)),
+        extensions: vec![runjs_extension],
         ..Default::default()
     });
     js_runtime
@@ -28,6 +61,8 @@ fn main() {
         .enable_all()
         .build()
         .unwrap();
+
+    // loads example.js to test the functionality that is added
     if let Err(error) = runtime.block_on(run_js("./example.js")) {
         eprintln!("error: {}", error);
     }
